7a9aa13065c41fb172e1c750f0df72e2725aef09
Lama <lama@gmail.com>
2026-05-20 20:00:50 +0300

    add comments

--- src/App.tsx
+++ src/App.tsx
@@ -22,9 +22,7 @@ function App() {
       <Route path="/" element={<Navigate to="/login" replace />} />
 
       {/* Protected */}
-      <Route
-        path="/dashboard"
-        element={
+      <Route path="/dashboard" element={
           <ProtectedRoute>
             <MainLayout />
           </ProtectedRoute>
